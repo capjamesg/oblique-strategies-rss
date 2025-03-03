f570b87787600d270289554f01edbb5d9a60075f
capjamesg <jamesg@jamesg.blog>
2025-03-03 11:54:39 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Oblique Strategies RSS
+
+An RSS feed for the Oblique Strategies deck of cards by Brian Eno and Peter Schmidt.
---
+++ daily.py
@@ -0,0 +1,34 @@
+import random
+from datetime import datetime
+
+import jinja2
+from granary import microformats2, rss
+import os
+
+FILE_DIR = os.path.dirname(os.path.realpath(__file__))
+OUTPUT_DIR = "/var/www/jamesg.blog/oblique-strategies/"
+
+with open(os.path.join(FILE_DIR, "strategies.txt"), "r") as f:
+    strategies = f.read().splitlines()
+
+with open(os.path.join(FILE_DIR, "template.html"), "r") as f:
+    template_file = f.read()
+
+template = jinja2.Template(template_file)
+
+rendered_template = template.render(
+    strategy=random.sample(strategies, 1)[0],
+    date=datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ"),
+)
+
+with open(os.path.join(OUTPUT_DIR, "index.html"), "w") as f:
+    f.write(rendered_template)
+
+rss = rss.from_activities(
+    microformats2.html_to_activities(rendered_template),
+    feed_url="https://jamesg.blog/oblique-strategies",
+    title="Oblique Strategies RSS",
+)
+
+with open(os.path.join(OUTPUT_DIR, "rss.xml"), "w") as f:
+    f.write(rss)
