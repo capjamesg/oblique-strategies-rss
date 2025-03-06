0c1bf10f95a528517e2bb18a01b0093243c149fc
James <jamesg@jamesg.blog>
2025-03-06 13:30:14 +0000

    add pwa

--- daily.py
+++ daily.py
@@ -6,7 +6,8 @@ from granary import microformats2, rss
 import os
 
 FILE_DIR = os.path.dirname(os.path.realpath(__file__))
-OUTPUT_DIR = "/var/www/jamesg.blog/oblique-strategies/"
+OUTPUT_DIR = "."
+# OUTPUT_DIR = "/var/www/jamesg.blog/oblique-strategies/"
 
 with open(os.path.join(FILE_DIR, "strategies.txt"), "r") as f:
     strategies = f.read().splitlines()
@@ -16,12 +17,29 @@ with open(os.path.join(FILE_DIR, "template.html"), "r") as f:
 
 template = jinja2.Template(template_file)
 
+strategy = "Intentions -credibility of -nobility of -humility of" # random.sample(strategies, 1)[0]
+if strategy.count(" -") > 1:
+    strategy = strategy.replace(" -", "<br> -")
+
 rendered_template = template.render(
-    strategy=random.sample(strategies, 1)[0],
+    strategy=strategy,
     date=datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ"),
 )
 
 with open(os.path.join(OUTPUT_DIR, "index.html"), "w") as f:
+    f.write(rendered_template)
+
+with open(os.path.join(FILE_DIR, "pwa.html"), "r") as f:
+    template_file = f.read()
+
+template = jinja2.Template(template_file)
+
+rendered_template = template.render(
+    strategy=strategy,
+    date=datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ"),
+)
+
+with open(os.path.join(OUTPUT_DIR, "pwa_out.html"), "w") as f:
     f.write(rendered_template)
 
 rss = rss.from_activities(
